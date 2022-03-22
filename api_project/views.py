26e12859a77eb227a3087a837f6fc6cebb752cfb
Nns <nns7007@gmail.com>
2022-03-22 23:59:17 +0530

    Added POST,PUT,PATCH & DELETE method

--- api_project/views.py
+++ api_project/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from api_project import serializers
 
 class HelloApiView(APIView):
     """Test API view"""
+    serializers_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Return features of APIview in list format"""
@@ -15,3 +18,29 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!','an_apiview': an_apiview})
+
+    def post(self,request):
+        """create hello msg with our name"""
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello{name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        """Update the object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """partially update the object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
