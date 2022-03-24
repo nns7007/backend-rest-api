23caccad62a7f5e99076b5a953255668a244c240
Nns <nns7007@gmail.com>
2022-03-24 12:41:20 +0530

    Added Login Api

--- api_project/views.py
+++ api_project/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from api_project import serializers
 from api_project import models
@@ -105,3 +107,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """create user auth tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
